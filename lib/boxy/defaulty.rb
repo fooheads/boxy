9ed2fc7c021e38ac6e682fa33171649fb5aeb31a
Niclas Nilsson <niclas@niclasnilsson.se>
2015-12-02 23:29:14 +0100

    Add support for specifying where defaulty should load it's files from, with ENV['DEFAULTY_URL']

--- lib/boxy/defaulty.rb
+++ lib/boxy/defaulty.rb
@@ -3,7 +3,8 @@ require 'defaulty'
 module Boxy
   class DefaultyHandler
     def install(domain, options)
-      @defaulty ||= Defaulty.load('https://api.github.com/repos/fooheads/osxdefaults/contents/')
+      defaulty_url = ENV['DEFAULTY_URL'] || 'https://api.github.com/repos/fooheads/osxdefaults/contents/'
+      @defaulty ||= Defaulty.load(defaulty_url)
       @defaulty.write(domain, options)
     end
   end
